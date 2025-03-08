1a7bb6ab50615ecb2e9e247bee0de4b6ccbd1555
Jesse van Assen <jesse.v.assen@gmail.com>
2025-03-08 14:31:50 +0100

    Add solution for 02

---
+++ src/bin/02.rs
@@ -0,0 +1,17 @@
+use std::{io::stdin, str::FromStr as _};
+
+use chrono::{DateTime, Utc};
+use itertools::Itertools;
+
+fn main() {
+	let answer = stdin().lines()
+		.map(Result::unwrap)
+		.map(|str| DateTime::<Utc>::from_str(&str).unwrap())
+		.counts()
+		.into_iter()
+		.filter_map(|(instant, count)| (count >= 4).then_some(instant))
+		.exactly_one()
+		.expect("Expected only one instant that occurs four or more times in the input");
+
+	println!("{}", answer.fixed_offset().to_rfc3339());
+}
